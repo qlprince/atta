f644f7c113afd8fa8c09cd781358906e23b98427
Troy Zhang <troy_d_zhang@homedepot.com>
2017-11-05 14:03:54 -0500

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatCardModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
+import {SideNav1ComponentComponent} from "./side-nav-1-component/side-nav-1-component.component";
+import {Main1ComponentComponent} from "./main-1-component/main-1-component.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    Main1ComponentComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
